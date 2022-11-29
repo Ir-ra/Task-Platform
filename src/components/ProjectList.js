6c7e40b7d57ff70de300475fd89c15bef3867775
Ir-ra <chapter.ku@ukr.net>
2022-11-29 15:32:58 +0200

    Project List

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,16 +1,29 @@
-
+import { Link } from 'react-router-dom';
+import Avatar from './Avatar';
 // styles
 import './ProjectList.css'
 
 function ProjectList({ projects }) {
 
     return (
-        <div>
+        <div className='project-list'>
             {/* in case there is no projects */}
             {projects.length === 0 && <p>No projects yet!</p>}
 
             {projects.map(project => (
-                <div key={project.id}>{project.name}</div>  //name це з Create.js де створювали сам project document (project)
+                <Link to={`/projects/${project.id}`} key={project.id}>
+                    <h4>{project.name} </h4>
+                    <p>Due by {project.dueDate.toDate().toDateString()}</p>
+                    <div className='assigned-to'>
+                        <ul>
+                            {project.assignedUsersList.map(user => (
+                                <li key={user.photoURL}>
+                                    <Avatar src={user.photoURL} /> 
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </Link>
 
             ))}
         </div>
