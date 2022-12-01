8cfcb7522e4772f7e3d0f81259798090e7cce300
Ir-ra <chapter.ku@ukr.net>
2022-12-01 11:01:22 +0200

    project Filters

--- src/pages/dashboard/DashBoard.js
+++ src/pages/dashboard/DashBoard.js
@@ -1,17 +1,54 @@
+import { useState } from 'react';
 import ProjectList from '../../components/ProjectList';
 import { useCollection } from '../../hooks/useCollection'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 // styles
 import './DashBoard.css'
+import ProjectFilter from './ProjectFilter';
 
 function DashBoard() {
+    const { user } = useAuthContext()
     const { documents, error } = useCollection('PROJECTs')
+    const [currentFilter, setCurrentFilter] = useState('all')
+
+    const changeFilter = (newFilter) => {
+        setCurrentFilter(newFilter)
+    }
+
+    const filteredProjects = documents ? documents.filter((document) => {
+        switch (currentFilter) {
+            case 'all':
+                return true
+            case 'mine':
+                let assignedToMe = false
+                document.assignedUsersList.forEach((u) => {
+                    if (user.uid === u.id) {
+                        assignedToMe = true
+                    }
+                })
+                return assignedToMe
+            case 'development':
+            case 'design':
+            case 'sales':
+            case 'marketing':
+                console.log(document.category, currentFilter)
+                return document.category === currentFilter
+            default:
+                return true
+        }
+    }) : null
 
     return (
         <div>
             <h2 className='page-title'>DashBoard</h2>
             {error && <p className='error'>{error}</p>}
-            {documents && <ProjectList projects={documents} />}
+            {documents && (
+                <ProjectFilter
+                    currentFilter={currentFilter}
+                    changeFilter={changeFilter} />
+            )}
+            {filteredProjects && <ProjectList projects={filteredProjects} />}
         </div>
     );
 }
